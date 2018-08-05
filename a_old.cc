bc6f23d61841d1c8d89b03aa0611bb9c7e717051
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2018-08-05 08:14:38 -0500

    11100 - The Trip, 2007

---
+++ a_old.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define assn(n,a,b) assert(n<=b and n>=a)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define SZ(a) (int)(a.size())
+#define SET(a,b) memset(a,b,sizeof(a))
+#define LET(x,a) __typeof(a) x(a)
+#define TR(v,it) for( LET(it,v.begin()) ; it != v.end() ; it++)
+#define repi(i,n) for(int i=0; i<(int)n;i++)
+#define sd(n) scanf("%d",&n)
+#define sl(n) scanf("%lld",&n)
+#define sortv(a) sort(a.begin(),a.end())
+#define all(a) a.begin(),a.end()
+#define DRT()  int t; cin>>t; while(t--)
+#define TRACE
+using namespace std;
+//FILE *fin = freopen("in","r",stdin);
+//FILE *fout = freopen("out","w",stdout);
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector< pii > vii;
+
+int n, m;
+int d[20004];
+int k[20004];
+
+//ios::sync_with_stdio(0); cin.tie(0);
+int main(void)
+{
+  while(sd(n), sd(m), n || m) {
+    repi(i, n) {
+      sd(d[i]);
+    }
+
+    repi(i, m) {
+      sd(k[i]);
+    }
+
+    sort(d, d+n);
+    sort(k, k+m);
+
+    int ans = 0;
+    int d_idx = 0, k_idx = 0;
+
+    while(d_idx < n && k_idx < m) {
+      while(d[d_idx] > k[k_idx] && k_idx < m)
+        k_idx++;
+
+      if(k_idx == m) 
+        break;
+      
+
+      ans += k[k_idx];
+
+      d_idx++;
+      k_idx++;
+    }
+
+    if(d_idx == n)
+      printf("%d\n", ans);
+    else
+      printf("Loowater is doomed!\n");
+  }
+}

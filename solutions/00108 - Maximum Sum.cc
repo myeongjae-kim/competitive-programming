f2596a4e6f575f132023a06285918cb1a50f89b5
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2018-08-20 00:08:26 -0500

    00108 - Maximum Sum.cc

--- solutions/00108 - Maximum Sum.cc
+++ solutions/00108 - Maximum Sum.cc
@@ -1,76 +1,39 @@
-#include <bits/stdc++.h>
-#define assn(n,a,b) assert(n<=b and n>=a)
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define SZ(a) (int)(a.size())
-#define SET(a,b) memset(a,b,sizeof(a))
-#define LET(x,a) __typeof(a) x(a)
-#define TR(v,it) for( LET(it,v.begin()) ; it != v.end() ; it++)
-#define repi(i,n) for(int i=0; i<(int)n;i++)
-#define sd(n) scanf("%d",&n)
-#define sl(n) scanf("%lld",&n)
-#define sortv(a) sort(a.begin(),a.end())
-#define all(a) a.begin(),a.end()
-#define DRT()  int t; cin>>t; while(t--)
-#define TRACE
+#include <iostream>
+#include <vector>
+#include <climits>
+
 using namespace std;
-//FILE *fin = freopen("in","r",stdin);
-//FILE *fout = freopen("out","w",stdout);
-#ifdef TRACE
-#define trace1(x)                cerr << #x << ": " << x << endl;
-#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
-#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
-#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
-#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
-#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
-#else
-#define trace1(x)
-#define trace2(x, y)
-#define trace3(x, y, z)
-#define trace4(a, b, c, d)
-#define trace5(a, b, c, d, e)
-#define trace6(a, b, c, d, e, f)
-#endif
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-typedef vector< pii > vii;
 
-//ios::sync_with_stdio(0); cin.tie(0);
-#include <stdio.h>
+int v[102][102];
 
-int ary[102][102];
+int main(void)
+{
+  ios::sync_with_stdio(0); cin.tie(0);
+  int N;
 
-int main(void) {
-  int n;
-
-  while(sd(n) != EOF) {
-    repi(i, n) {
-      repi(k, n) {
-        sd(ary[i][k]);
-        if(i>0) ary[i][k] += ary[i-1][k];
-        if(k>0) ary[i][k] += ary[i][k-1];
-        if(i>0 && k>0) ary[i][k] -= ary[i-1][k-1];
+  while(cin >> N) {
+    for (int i = 0; i < N; ++i) {
+      for (int k = 0; k < N; ++k) {
+        cin >> v[i][k]; 
+        if(k > 0) v[i][k] += v[i][k-1];
       }
     }
 
-    int local_ans;
-    int ans = INT_MIN;
-    repi(i, n) {
-      repi(k, n) {
-        for(int r = 0; i + r < n; r++) {
-          for(int c = 0; k + c < n; c++) {
-            local_ans = ary[i+r][k+c];
-            if(i > 0) local_ans -= ary[i-1][k+c];
-            if(k > 0) local_ans -= ary[i+r][k-1];
-            if(i>0 && k>0) local_ans += ary[i-1][k-1];
-            ans = max(ans, local_ans);
-          }
+    int ans = INT_MIN, local_ans;
+
+    for (int c_from = 0; c_from < N; c_from++)
+      for (int c_to = c_from; c_to < N; c_to++) {
+        local_ans = 0;
+        for(int i = 0; i < N; i++) {
+          local_ans += v[i][c_to];
+          if(c_from > 0) local_ans -= v[i][c_from-1];
+
+          ans = max(ans, local_ans);
+
+          if(local_ans < 0) local_ans = 0;
         }
       }
-    }
+
     printf("%d\n", ans);
   }
 

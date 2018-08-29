e853e3b7227a7885e680199b6c403cb3bbd4ea2c
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2018-08-29 08:07:39 -0500

    216 - Getting in Line

---
+++ temp.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+  double a = INFINITY;
+
+  cout << min(a, 10.0) << endl;
+  
+  return 0;
+}
